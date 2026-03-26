a9ef30e32d22a96b41ad6b50001c39b459a830e5
Miguel Ferreira <ferreira@dinfor.uniceub>
2026-03-25 21:44:22 -0300

    class about vue

---
+++ Miguel Ferreira/Aulas/vue_cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <home-componente/>
+    <hr/>
+    <home-componente/>
+  </div>
+</template>
+
+<script>
+import HomeComponente from './components/HomeComponente.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeComponente
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Miguel Ferreira/Aulas/vue_cli/src/components/HomeComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <footer>
+            <p>Bem-vindo! {{ nome }}, Aproveite o código <b>Vue com CLI</b></p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeComponente',
+        data() {
+            return {
+                nome: "Miguel"
+            };
+        },
+    };
+</script>
